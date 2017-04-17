908573c0400619c527abb3988428215dc4955cde
Jose A. Álvarez <infoiguacel@gmail.com>
2017-04-17 15:16:09 +0200

    Update index.js

--- js/index.js
+++ js/index.js
@@ -1,72 +1,66 @@
 // var
-var sec = $('section').height();
+var sec = $("section").height();
 // init Isotope
-var $grid = $('.grid').isotope({
+var $grid = $(".grid").isotope({
     // options
-    itemSelector: '.element-item',
+    itemSelector: ".element-item"
 });
 // filter items on button click
-$('.col').on('click', 'svg', function() {
+$(".col").on("click", "svg", function() {
     $(this).addClass("rotar");
-    $('section').css("display", "block");
-    $('.muestraTodo').fadeIn('fast', function() {});
-    var filterValue = $(this).attr('data-filter');
+    $("section").css("display", "block");
+    $(".muestraTodo").fadeIn("fast", function() {});
+    var filterValue = $(this).attr("data-filter");
     $grid.isotope({ filter: filterValue });
     if (filterValue == ".special") {
         console.log(filterValue);
-        $('.features').css("display", "none");
-        $('.breakingNews').css("display", "none");
-        $('.criteria').css("display", "none");
-        $('.socialMediaGraphics').css("display", "none");
-        $('.specials').css("display", "none");
+        $(".features").css("display", "none");
+        $(".breakingNews").css("display", "none");
+        $(".criteria").css("display", "none");
+        $(".socialMediaGraphics").css("display", "none");
+        $(".specials").css("display", "none");
     }
     if (filterValue == ".gold") {
         console.log(filterValue);
-        $('.breakingNews').css("display", "none");
-        $('.specialAwards').css("display", "none");
-        $('.criteria').css("display", "none");
-        $('.specials').css("display", "none");
+        $(".breakingNews").css("display", "none");
+        $(".specialAwards").css("display", "none");
+        $(".criteria").css("display", "none");
+        $(".specials").css("display", "none");
     }
     if (filterValue == ".silver") {
         console.log(filterValue);
-        $('.specialAwards').css("display", "none");
-        $('.specials').css("display", "none");
+        $(".specialAwards").css("display", "none");
+        $(".specials").css("display", "none");
     }
     if (filterValue == ".bronze") {
         console.log(filterValue);
-        $('.specialAwards').css("display", "none");
+        $(".specialAwards").css("display", "none");
     }
     if (filterValue == ".usa") {
         console.log(filterValue);
-        $('.specials').css("display", "none");
+        $(".specials").css("display", "none");
     }
     if (filterValue == ".uk") {
         console.log(filterValue);
-        $('.criteria').css("display", "none");
-        $('.specials').css("display", "none");
+        $(".criteria").css("display", "none");
+        $(".specials").css("display", "none");
     }
     if (filterValue == ".other") {
         console.log(filterValue);
-        $('.specialAwards').css("display", "none");
-        $('.socialMediaGraphics').css("display", "none");
+        $(".specialAwards").css("display", "none");
+        $(".socialMediaGraphics").css("display", "none");
     }
     if (filterValue == ".less") {
         console.log(filterValue);
-        $('.specialAwards').css("display", "none");
+        $(".specialAwards").css("display", "none");
     }
     if (filterValue == ".more") {
         console.log(filterValue);
-        $('.specials').css("display", "none");
+        $(".specials").css("display", "none");
     }
 });
-$('.muestraTodo').on('click', 'button', function() {
-    var filterValue = $(this).attr('data-filter');
+$(".muestraTodo").on("click", "button", function() {
+    $("section").css("display", "block");
+    var filterValue = $(this).attr("data-filter");
     $grid.isotope({ filter: filterValue });
 });
-// OCULTAR
-// $('.features').css("display", "none");
-// $('.breakingNews').css("display", "none");
-// $('.specialAwards').css("display", "none");
-// $('.criteria').css("display", "none");
-// $('.socialMediaGraphics').css("display", "none");
-// $('.specials').css("display", "none");
